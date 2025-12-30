712ab5217757c82c20dd08714febc4a25266917b
milan1990 <hamed.moyasseri2012@gmail.com>
2025-12-30 12:12:23 +0330

    v2

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-const counter = document.getElementById(counter);
-const Minus = document.getElementById(Minus);
-const Reset = document.getElementById(Reset);
-const Plus = document.getElementById(Plus);
-let cont = 0;
+const counter = document.getElementById("counter");
+const Minus = document.getElementById("Minus-btn");
+const Reset = document.getElementById("Reset-btn");
+const Plus = document.getElementById("Plus-btn");
+let count = 0;
 Minus.addEventListener("click", () => {
-  Cont -= 1;
-  Cont.textContent = Cont;
-  console.log(conter);
-  if (cont < 0) {
+  count -= 1;
+  count.textContent = count;
+  console.log(counter);
+  if (count < 0) {
     counter.style.color = "Red";
   }
 });
 Plus.addEventListener("click", () => {
-  cont += 1;
-  Cont.textContent = Cont;
+  count += 1;
+  count.textContent = count;
   console.log(counter);
-  if (cont > 0) {
+  if (count > 0) {
     counter.style.color = "Green";
   }
 });
 Reset.addEventListener("click", () => {
-  cont = 0;
-  cont.textContent = Cont;
-  conscole.log(conter);
-  if (Cont == 0) {
-    counter.style.color = gray;
+  count = 0;
+  count.textContent = count;
+  conscole.log("counter");
+  if (count == 0) {
+    counter.style.color = "gray";
   }
 });
